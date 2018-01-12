affbe5849b93f667c23f72a0f5b3257878056140
Unknown <sbsmith@beckman.com>
2018-01-12 15:08:06 -0500

    Test App Created

---
+++ Model/TextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Model
+{
+    public class TextConverter
+    {
+        private readonly Func<string, string> _conversion;
+
+        public TextConverter(Func<string, string> conversion)
+        {
+            _conversion = conversion;
+        }
+
+        public string ConvertText(string inputText)
+        {
+            return _conversion(inputText);
+        }
+    }
+}
